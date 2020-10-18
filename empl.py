c97ef5ff048822ac8fed2f862fdf0529d6d97d3c
yushanttyagi <yushanttyagi@gmail.com>
2020-10-19 02:49:49 +0530

    first commit

---
+++ empl.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template
+from cassandra.cluster import Cluster
+import os
+import socket
+def cassandra_connection():
+	"""
+	Connection object for Cassandra
+	:return: session, cluster
+	"""
+	#cluster = Cluster(['172.19.0.2'],port=9042)
+	cluster = Cluster()
+	session = cluster.connect()
+	session.execute("""CREATE KEYSPACE IF NOT EXISTS quantiphi WITH REPLICATION={'class':'SimpleStrategy','replication_factor':1} AND durable_writes='true' """)
+	session.set_keyspace('quantiphi')
+	session.execute("create table if not exists employee(id int primary key,name text,address text)")
+	return session,cluster
+
+
+#init app
+app=Flask(__name__)
+
+#session,cluster=cassandra_connection()
+
+@app.route("/")
+def pri():
+	return render_template("employee.html",title="employee registration")
+
+
+@app.route("/register",methods=["POST"])
+def register():
+	idd=request.form["idd"]
+	name=request.form["name"]
+	address=request.form["address"]
+	try:
+		print(idd,name,address)
+		hostname = socket.gethostname()    
+		IPAddr = socket.gethostbyname(hostname) 
+		#session.execute("insert into employee(id,name,address) values({},'{}','{}')".format(idd,name,address))
+		return 'written successfull {}----{}'.format(hostname,IPAddr)
+	except Exception as e:
+		return e
+
+
+if __name__=="__main__":
+	app.run(host='0.0.0.0',port=5001,debug = True)
